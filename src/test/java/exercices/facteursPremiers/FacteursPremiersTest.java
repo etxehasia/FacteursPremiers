bcea6e6938b3a2a089b6bdc09bc8779658185323
Pantxika Dagorret <pantxika.dagorret@gmail.com>
2023-09-29 12:06:09 +0200

    gradle paramétré (jAssert) + packages créés + classe test créée + imports JUnit JAssert ok

---
+++ src/test/java/exercices/facteursPremiers/FacteursPremiersTest.java
@@ -0,0 +1,13 @@
+package exercices.facteursPremiers;
+
+/*
+L'instruction : import org.junit.jupiter.api.*     inclut (pour notre code) :
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+ */
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+public class FacteursPremiersTest {
+}
